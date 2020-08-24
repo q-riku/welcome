fc9c8b87923f222af5ee2ce9eed04eecaf4e294d
q-riku <q-riku@users.noreply.github.com>
2020-08-24 15:00:19 +0900

    welcome page

---
+++ script.js
@@ -0,0 +1,43 @@
+$('section').hide();
+var i = 0;
+var txt = "w e l c o m e";
+var welcome = document.getElementById('welcome');
+
+//type animation
+function type() {
+    welcome.innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(type, 150);
+}
+type();
+
+//title animation
+function move() {
+    $('#welcome').animate({
+        top: '-100px'
+    }, 600);
+}
+setTimeout(move, 2000);
+
+//button animation
+function show() {
+    $('section').fadeIn(600);
+}
+setTimeout(show, 2600);
+
+//button effects
+const buttons = document.querySelectorAll('.button');
+buttons.forEach(function (btn) {
+    btn.addEventListener('mouseenter', function (e) {
+        let x = e.clientX - e.target.offsetLeft;
+        let y = e.clientY - e.target.offsetTop;
+        let ripples = document.createElement('span');
+        ripples.style.left = x + 'px';
+        ripples.style.top = y + 'px';
+        this.appendChild(ripples);
+
+        setTimeout(function(){
+            ripples.remove()
+        },800);
+    });
+});
